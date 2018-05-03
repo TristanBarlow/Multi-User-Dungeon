efcfef996cdda42cb018e8b07eb6f250713f2258
tristanbarlowg@gmail.com <Samandfrodo12>
2018-05-03 17:27:52 +0100

    Added the action response class

---
+++ Server/windows/ActionResponsecs.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Server
+{
+    public enum ActionID {SAY, MOVE, NORMAL, UPDATE };
+    public class ActionResponse
+    {
+        public ActionID ID = ActionID.NORMAL; 
+        public string message = "";
+        public void Set(String str, ActionID id)
+        {
+            ID = id;
+            message = str;
+        }
+    }
+
+}
